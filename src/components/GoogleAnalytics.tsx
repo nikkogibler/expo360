be93c3908b133a85035425df91ef2c09644d5212
nikkogibler <dev@interzekt.com>
2026-01-08 18:12:20 +0100

    Simplify GA4 init with onLoad callback

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,47 +1,32 @@
 'use client';
 
 import Script from 'next/script';
-import { useEffect } from 'react';
 import { GA_MEASUREMENT_ID } from '../utils/googleAnalytics';
 
-declare global {
-  interface Window {
-    gtag: (...args: unknown[]) => void;
-    dataLayer: unknown[];
-  }
-}
-
 export default function GoogleAnalytics() {
-  // Initialize GA4 after script loads
-  useEffect(() => {
-    if (GA_MEASUREMENT_ID && typeof window !== 'undefined' && window.gtag) {
-      window.gtag('config', GA_MEASUREMENT_ID, {
-        page_location: window.location.href,
-        page_path: window.location.pathname + window.location.search,
-      });
-    }
-  }, []);
-
   // Return null if measurement ID is not configured
   if (!GA_MEASUREMENT_ID) {
     return null;
   }
 
+  const handleScriptLoad = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(...args: unknown[]) {
+      window.dataLayer.push(args);
+    }
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', GA_MEASUREMENT_ID, {
+      page_location: window.location.href,
+      page_path: window.location.pathname + window.location.search,
+    });
+  };
+
   return (
-    <>
-      {/* Google tag (gtag.js) */}
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}');
-        `}
-      </Script>
-    </>
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      strategy="afterInteractive"
+      onLoad={handleScriptLoad}
+    />
   );
 }
