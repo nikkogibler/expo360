c592cd965db2e7098271cce10ecc32a53747fb99
nikkogibler <dev@interzekt.com>
2026-01-08 18:29:14 +0100

    Revert GA4 to standard config - UTM params read automatically by GA4

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -4,62 +4,24 @@ import Script from 'next/script';
 import { GA_MEASUREMENT_ID } from '../utils/googleAnalytics';
 
 export default function GoogleAnalytics() {
-  // Return null if measurement ID is not configured
   if (!GA_MEASUREMENT_ID) {
     return null;
   }
 
-  const handleScriptLoad = () => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(...args: unknown[]) {
-      window.dataLayer.push(args);
-    }
-    window.gtag = gtag;
-    gtag('js', new Date());
-
-    // Parse UTM parameters from URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const utmSource = urlParams.get('utm_source');
-    const utmMedium = urlParams.get('utm_medium');
-    const utmCampaign = urlParams.get('utm_campaign');
-
-    // Disable automatic page view - we'll send it manually with UTM params
-    gtag('config', GA_MEASUREMENT_ID, {
-      send_page_view: false,
-    });
-
-    // Send page view event with UTM parameters explicitly set
-    const eventParams: Record<string, string> = {
-      page_location: window.location.href,
-      page_path: window.location.pathname + window.location.search,
-      page_title: document.title,
-    };
-
-    // Add campaign parameters if present
-    if (utmSource) {
-      eventParams.campaign_source = utmSource;
-      eventParams.source = utmSource;
-    }
-    if (utmMedium) {
-      eventParams.campaign_medium = utmMedium;
-      eventParams.medium = utmMedium;
-    }
-    if (utmCampaign) {
-      eventParams.campaign_name = utmCampaign;
-      eventParams.campaign = utmCampaign;
-    }
-
-    // Log for debugging
-    console.log('📊 GA4 page_view with params:', eventParams);
-
-    gtag('event', 'page_view', eventParams);
-  };
-
   return (
-    <Script
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      strategy="afterInteractive"
-      onLoad={handleScriptLoad}
-    />
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
   );
 }
